d3ccbced24fe6bff48e4cf38478fe6f5988d48ea
YumYumeYum <max.rauchbach2002@gmail.com>
2024-10-14 18:22:32 +0200

    erste anfaenge AB5

--- Aufgabe5/Sortierung.java
+++ Aufgabe5/Sortierung.java
@@ -1,5 +1,18 @@
 public class Sortierung{
     static void mergesort(Integer[] a){
 
+        if(a.length == 1){
+            return;
+        }
+
+        Integer half_length = a.length / 2;     //rundet bei ungeraden Zahlen
+        Integer[] firstHalf = new Integer[half_length];
+        Integer[] secondHalf = new Integer[a.length - half_length];
+
+
+        
     }
 }
+
+//e1, e2, e3 |index| e4, e5, e6
+//        <--   i   -->
