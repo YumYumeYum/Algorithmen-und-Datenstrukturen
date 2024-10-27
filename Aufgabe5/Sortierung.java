77de0b23491fd11ff17a5e1fbe06879b573e540c
Maximillian Rauchbach <yume@fedora.fritz.box>
2024-10-27 12:31:39 +0100

    fortschritt AB5

--- Aufgabe5/Sortierung.java
+++ Aufgabe5/Sortierung.java
@@ -5,26 +5,47 @@ public class Sortierung{
 
         if(a.length > 1){
             Integer half_length = a.length / 2;     //rundet bei ungeraden Zahlen
-            Integer[] firstHalf = new Integer[half_length];
-            Integer[] secondHalf = new Integer[a.length - half_length];
+            Integer[] left = new Integer[half_length];
+            Integer[] right = new Integer[a.length - half_length];
 
-            System.arraycopy(a, 0, firstHalf, 0, firstHalf.length);
-            System.out.println(Arrays.toString(firstHalf));
+            System.arraycopy(a, 0, left, 0, left.length);
+            System.out.println(Arrays.toString(left));
 
-            System.arraycopy(a, half_length, secondHalf, 0, secondHalf.length);
-            System.out.println(Arrays.toString(secondHalf));
+            System.arraycopy(a, half_length, right, 0, right.length);
+            System.out.println(Arrays.toString(right));
 
-            //sortieren
-            
+            mergesort(left);
+            mergesort(right);
 
+            merge(left, right);
 
-
-            //mergen
         }
-
-        
     }
+
+    static Integer[] merge(Integer[] left, Integer[] right){
+
+
+        //initialisierung
+        int arr_length = left.length + right.length;
+        int i = 0;
+        int j = left.length;
+        Integer[] merged_Array = new Integer[arr_length];
+
+        //Logik
+
+
+        return merged_Array;
+    }
+
+    
 }
 
 //e1, e2, e3 |index| e4, e5, e6
 //        <--   i   -->
+
+/*commit Nachricht:
+change array name: left,right naming(first,second before),
+
+
+
+*/
