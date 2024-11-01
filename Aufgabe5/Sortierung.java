7bae6a26fbf1a307e5628cd8d101645fa8c1ca46
Max-Fedora <max.rauchbach2002@gmail.com>
2024-11-01 02:00:28 +0100

    AB5 Nr1 feritg

--- Aufgabe5/Sortierung.java
+++ Aufgabe5/Sortierung.java
@@ -4,48 +4,42 @@ public class Sortierung{
     static void mergesort(Integer[] a){
 
         if(a.length > 1){
-            Integer half_length = a.length / 2;     //rundet bei ungeraden Zahlen
+            int half_length = a.length / 2;     //rundet bei ungeraden Zahlen
             Integer[] left = new Integer[half_length];
             Integer[] right = new Integer[a.length - half_length];
 
             System.arraycopy(a, 0, left, 0, left.length);
-            System.out.println(Arrays.toString(left));
+            System.out.println("left = " + Arrays.toString(left));
 
             System.arraycopy(a, half_length, right, 0, right.length);
-            System.out.println(Arrays.toString(right));
+            System.out.println("right = " + Arrays.toString(right));
 
             mergesort(left);
             mergesort(right);
 
-            merge(left, right);
+            merge(a, left, right, half_length,a.length - half_length );
 
         }
     }
 
-    static Integer[] merge(Integer[] left, Integer[] right){
+    static void merge(Integer[] a, Integer[] left, Integer[] right, int mid, int end) {
+ 
+        int i = 0, j = 0, k = 0;
 
-
-        //initialisierung
-        int arr_length = left.length + right.length;
-        int i = 0;
-        int j = left.length;
-        Integer[] merged_Array = new Integer[arr_length];
-
-        //Logik
-
-
-        return merged_Array;
+        while (i < mid && j < end) {
+            if (left[i] <= right[j]) {
+                a[k++] = left[i++];
+            }
+            else {
+                a[k++] = right[j++];
+            }
+        }
+        while (i < mid) {
+            a[k++] = left[i++];
+        }
+        while (j < end) {
+            a[k++] = right[j++];
+        }
+        System.out.println("merge (a) = " + Arrays.toString(a));
     }
-
-    
 }
-
-//e1, e2, e3 |index| e4, e5, e6
-//        <--   i   -->
-
-/*commit Nachricht:
-change array name: left,right naming(first,second before),
-
-
-
-*/
